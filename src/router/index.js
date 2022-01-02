7be2fe93b6223cacca48ed8b45f1b01d130534be
raindops <raindopshe@tencent.com>
2022-01-02 15:50:22 +0800

    添加报表功能echart

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import GoodsList from '../components/goods/List.vue'
 import GoodsAdd from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(VueRouter)
 
@@ -73,6 +74,11 @@ const routes = [
         path: '/orders',
         name: 'Order',
         component: Order
+      },
+      {
+        path: '/reports',
+        name: 'Report',
+        component: Report
       }
     ]
   }
